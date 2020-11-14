1b05a9959045a0332351b8c3a7de9bb070702f33
Andrei Kozel <kozel.andrei.94@gmail.com>
2020-11-14 12:11:31 +0100

    new: functions ex6 is completed

--- w3resource/w3resource/Functions.cs
+++ w3resource/w3resource/Functions.cs
@@ -8,7 +8,24 @@ namespace w3resource
     {
         public void Exercise6()
         {
+            Console.WriteLine("Write a program in C# Sharp to create a function to swap the values of two integer numbers.");
+            Console.Write("First Number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"First: {firstNumber}, Second: {secondNumber}");
+            Console.WriteLine("Swaping ...");
+            
+            // The ref keyword indicates a value that is passed by reference.
+            // read more https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref
+            swapNumbers(ref firstNumber, ref secondNumber);
 
+            Console.WriteLine($"First: {firstNumber}, Second: {secondNumber}");
+        }
+
+        private void swapNumbers(ref int firstNumber, ref int secondNumber)
+        {
+            (firstNumber, secondNumber) = (secondNumber, firstNumber);
         }
 
         public void Exercise7()
