6859d10b91acdb012284721e987191697db8544c
Andrei Kozel <kozel.andrei.94@gmail.com>
2020-11-16 18:34:21 +0100

    new: övningsuppgift del 3 1 completed

---
+++ ÖvningsuppgifterDel3.1/ÖvningsuppgifterDel3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ÖvningsuppgifterDel3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 10 };
+            int i = 0;
+            while(i < arr.Length)
+            {
+                Console.Write($"{arr[i]} ");
+                i++;
+            }
+        }
+    }
+}
