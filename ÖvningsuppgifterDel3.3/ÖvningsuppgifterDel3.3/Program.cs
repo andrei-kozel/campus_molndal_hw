3259c5db58404ee94262cd940fc5d65d43503186
Andrei Kozel <kozel.andrei.94@gmail.com>
2020-11-16 19:04:07 +0100

    new: övningsuppgift del 3 3 completed

---
+++ ÖvningsuppgifterDel3.3/ÖvningsuppgifterDel3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ÖvningsuppgifterDel3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //13.Skapa en programm som frågar användaren efter ett heltal. 
+            //14.Skriv sedan en loop sprarar det talets gångentabell(1 till 10) i en array.
+            //15.Skriv sedan ut arrayen till consollen.
+
+            Console.WriteLine("frågar användaren efter ett heltal");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[10];
+            int index = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                arr[index] = num * i;
+                index++;
+            }
+            index = 0;
+            while(index < arr.Length)
+            {
+                Console.Write($"{arr[index]} ");
+                index++;
+            }
+
+        }
+    }
+}
